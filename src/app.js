1bd69c7c61a5a2f8b896ae37e41c591b0bd00315
m6ttw <matthewrhyswilliams@live.com>
2021-01-12 16:31:19 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -4,18 +4,47 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            countriesURL: null // https://restcountries.eu/rest/v2/all
+            countries: [], // https://restcountries.eu/rest/v2/all
+            selectedCountry: "",
         },
-        mounted: function() {
+        mounted() {
             this.fetchCountries();
         },
         methods: {
             fetchCountries: function() {
                 fetch("https://restcountries.eu/rest/v2/all")
                     .then(response => response.json())
-                    .then(data => this.countriesImgURL = data.name);
-                console.log(fetchCountries)
+                    .then(countries => this.countries = countries);
+            },
+            saveCountry: function() {
+                this.selectedCountry.push({
+                    name: this.selectedCountry,
+                    population: this.selectedCountry
+                });
             }
-        }
-    })
+        },
+
+        // methods: {
+        //     saveAccount: function(){
+        //       this.accounts.unshift(this.newAccount);
+
+        //       this.newAccount = {
+        //         name: "",
+        //         balance: 0
+        //       };
+        //     }
+        //   }
+
+        // computed: {
+        //     totalPopulation: function(){
+        //         return this.countries.reduce(total, country) => total + country.population);
+        //     },
+        //     filteredCountries: function(){
+        //         return this.countries.filter((country.name) => {
+        //             return country.name              
+        // })    
+        // }
+        // }
+        //          
+    });
 })
